273d0bc2f8b3adc61b0cefb342b5feb117d243b9
nazmul-1117 <nazmulonline2002@gmail.com>
2024-06-25 13:26:10 +0600

    dimik-summation

---
+++ dimik-divisor.cpp
@@ -0,0 +1,26 @@
+
+//Unsolved Problem
+
+#include <bits/stdc++.h>
+#define lld long long int
+
+using namespace std;
+
+int32_t main(){
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int16_t T;
+
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; ++i){
+    	if (49%i == 0){
+    			cout << i << " ";
+    		}
+    }
+
+  
+}
